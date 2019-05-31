811af704970d39bcc987bf86be5ad2bd27de3cde
DmitryZagorodnov <43351927+DmitryZagorodnov@users.noreply.github.com>
2019-05-31 20:55:52 +0300

    Delete BinaryTreeElement.kt

--- BinaryTreeElement.kt
+++
@@ -1,7 +0,0 @@
-public class BinaryTreeElement (
-        var element: Int,
-        var leftChild: BinaryTreeElement?,
-        var rightChild: BinaryTreeElement?) {
-    fun isNull(a: BinaryTreeElement): Boolean = a.element == null
-    fun isNotNull (a: BinaryTreeElement): Boolean = a.element != null
-}
